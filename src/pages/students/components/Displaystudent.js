7d54cdd23fe01a4923674556efafec40b9075428
Sandhya-Kanaujiya <sandhya.kanaujia.23espark@gmail.com>
2023-06-22 18:30:18 +0530

    22june..

--- src/pages/students/components/Displaystudent.js
+++ src/pages/students/components/Displaystudent.js
@@ -2,84 +2,136 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 
-
 const baseURL = "http://localhost:5454";
 const Displaystudent = () => {
 
   const [data, setData] = useState([]);
   const [totalRows, setTotalRows] = useState(1);
-  const options=[10,20]
+  const options = [10, 20]
   const [perPage, setPerPage] = useState(options);
-  //console.log(options,"oo")
+  const [searchQuery, setSearchQuery] = useState("");
 
- //const[options,setOptions]=useState(10);
   const fetchUsers = async page => {
-   
-    const response = await axios.get(`http://localhost:5454/student/displaydata/?page=${page}&limit=${perPage}`);
-
+    const response = await axios.get(`${baseURL}/student/displaydata/?page=${page}&limit=${perPage}`);
     setData(response.data.rows);
-   //console.log(response.data,"data")
-    setTotalRows(response.data);
-    //setTotalRows(response.data.rows);
-    //console.log(response.data.rows,"tt")
-    console.log(totalRows,"ll")
-};
-
-const handlePageChange = page => {
-    fetchUsers(page);
-};
-
-const handlePerRowsChange = async (newPerPage, page) => {
+    setTotalRows(response.data.count);
    
-    const response = await axios.get(`http://localhost:5454/student/displaydata/?page=${page}&limit=${newPerPage}`);
+  };
+  //console.log(data,"displaydata")
+  const handlePageChange = async (page) => {
+    if (!searchQuery) {
+      fetchUsers(page);
+    } else {
+      const response = await axios.get(
+        `${baseURL}/student/searchdata/${searchQuery}?page=${page}&limit=${perPage}`
+      );
+      setData(response.data.rows);
+      setTotalRows(response.data.count);
+    }
+  };
 
+  const handlePerRowsChange = async (newPerPage, page) => {
+    const response = await axios.get(`${baseURL}/student/displaydata/?page=${page}&limit=${newPerPage}`);
     setData(response.data.rows);
     setPerPage(newPerPage);
-};
+  };
 
-useEffect(() => {
-    fetchUsers(1); // fetch page 1 of users
-}, []);
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-const columns = [
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    if (!searchQuery) return fetchUsers(1);
+    const response = await axios.get(
+      `${baseURL}/student/searchdata/${searchQuery}?page=1&limit=${perPage}`
+    );
+    //console.log(response.data.rows,"dataasearch")
+    setData(response.data.rows);
+    setTotalRows(response.data.count);
+  };
+
+  useEffect(() => {
+    fetchUsers(1); 
+  }, []);
+  console.log(data,"alldata")
+  const columns = [
     {
-        name: "id",
-        selector: (row) => row.id,
-        sortable: true,
-      },
+      name: "id",
+      selector: (row) => row.id,
+      sortable: true,
+      sortField: 'id',
+    },
     {
       name: "firstname",
-      selector: (row) => row.firstname,
+      selector: (row) => row.student.firstname,
       sortable: true,
+      //sortField: 'firstname',
     },
     {
       name: "lastname",
-      selector: (row) => row.lastname,
+      selector: (row) => row.student.lastname,
       sortable: true,
+      //sortField: 'firstname',
     },
     {
-      name: "phone",
-      selector: (row) => row.phone,
+      name: "title",
+      selector: (row) => row.course.title,
       sortable: true,
+      sortField: 'title',
     },
+    // {
+    //   name: "firstname",
+    //   selector: (row) => row.firstname,
+    //   sortable: true,
+    // },
+    // {
+    //   name: "lastname",
+    //   selector: (row) => row.lastname,
+    //   sortable: true,
+    // },
+    // {
+    //   name: "phone",
+    //   selector: (row) => row.phone,
+    //   sortable: true,
+    // },
   ];
-return (
-    <DataTable
+
+  const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null,3)}</pre>;
+
+  // sorting
+  //const [data, setData] = useState(data);
+
+    const handleSort = async (column, sortDirection) => {
+    /// reach out to some API and get new data using or sortField and sortDirection
+    // e.g. https://api.github.com/search/repositories?q=blog&sort=${column.sortField}&order=${sortDirection}
+
+      //setData(remoteData);
+    };
+
+  
+  return (
+    <div>
+      <form onSubmit={handleSearchSubmit}>
+        <input type="text" value={searchQuery} onChange={handleSearch} placeholder="Search..." />
+        <button type="submit">Search</button>
+      </form>
+      <DataTable
         title="Users"
         columns={columns}
         data={data}
-        // progressPending={loading}
         pagination
-        //paginationComponentOptions={options}
         paginationRowsPerPageOptions={options}
         paginationServer
         paginationTotalRows={totalRows}
-
         onChangeRowsPerPage={handlePerRowsChange}
         onChangePage={handlePageChange}
-        
-    />
-);
+        expandableRows expandableRowsComponent={ExpandedComponent}
+        onSort={handleSort}
+        sortServer
+      />
+    </div>
+  );
 }
 
 export default Displaystudent
