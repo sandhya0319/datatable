beef0f2c4a2ef7af7f262983a97c1a1caa159b90
Sandhya-Kanaujiya <sandhya.kanaujia.23espark@gmail.com>
2023-06-23 17:46:20 +0530

    sort bug solve

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,94 +1,64 @@
-const db = require('../models');
-const { sequelize } = require('../models');
-const { Op } = require('sequelize');
-
+const db = require("../models");
+const { sequelize } = require("../models");
+const { Op } = require("sequelize");
 
 const student = db.student;
-const enrollment=db.enrollment;
-const course=db.course;
-const professor=db.professor;
-const comment=db.comment;
+const enrollment = db.enrollment;
+const course = db.course;
+const professor = db.professor;
+const comment = db.comment;
 
 const searchdata = async (req, res) => {
   const t = await db.sequelize.transaction();
   try {
     let { search } = req.params;
     // search.split(" ")
-    const page = parseInt(req.query.page) || 1; 
-    const limit = parseInt(req.query.limit) || 5; 
-    const offset = (page - 1) * limit; 
-    let searchData = await enrollment.findAndCountAll({
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = (page - 1) * limit;
+
+    const searchTerms = search.split(" ");
+
+    const searchData = await student.findAll({
       include: [
         {
-            model: student
-        },
-        {
-            model:course 
+          model: enrollment,
+          include: [
+            {
+              model: course,
+              include: {
+                model: professor,
+              },
+            },
+          ],
         },
       ],
       where: {
-        [Op.or]: [
-          sequelize.where(
-            sequelize.fn('LOWER', sequelize.col('firstname')),
-            'LIKE',
-            `%${search.toLowerCase()}%`
-          ),
-          sequelize.where(
-            sequelize.fn('LOWER', sequelize.col('lastname')),
-            'LIKE',
-            `%${search.toLowerCase()}%`
-          ),
-          sequelize.where(
-            sequelize.fn('LOWER', sequelize.col('phone')),
-            'LIKE',
-            `%${search.toLowerCase()}%`
-          ),
-        ],
+        [Op.and]: searchTerms.map((term) => ({
+          [Op.or]: [
+            sequelize.where(
+              sequelize.fn("LOWER", sequelize.col("firstname")),
+              "LIKE",
+              `%${term.toLowerCase()}%`
+            ),
+            sequelize.where(
+              sequelize.fn("LOWER", sequelize.col("lastname")),
+              "LIKE",
+              `%${term.toLowerCase()}%`
+            ),
+            sequelize.where(
+              sequelize.fn("LOWER", sequelize.col("phone")),
+              "LIKE",
+              `%${term.toLowerCase()}%`
+            ),
+          ],
+        })),
       },
       offset: offset,
       limit: limit,
     });
-    res.json(searchData);
-    await t.commit();
-  } catch (error) {
-    res.send(error);
-    await t.rollback();
-  }
-
-}
-
-const displaydata = async (req, res) => {
-  const t = await db.sequelize.transaction();
-  try {
-    const page = parseInt(req.query.page) || 1; 
-    const limit = parseInt(req.query.limit) || 5;
-    const offset = (page - 1) * limit; 
-
-    const Data = await enrollment.findAndCountAll({
-      include: [
-        {
-            model: student,
-            attributes:[
-              "firstname","lastname","phone"
-            ]
-        },
-        {
-            model:course,
-            // include: professor,
-            attributes:[
-              "title"
-            ]
-        },
-      ],
-      offset: offset,
-      limit: limit,
-      // order: [
-      //   ['firstname', 'DESC'],
-      // ],
-      // Add 
-      attributes:["id"]});
-
-    res.json(Data);
+    const totalcount = await student.count();
+    res.json({ searchData, totalcount });
     await t.commit();
   } catch (error) {
     res.send(error);
@@ -96,20 +66,100 @@ const displaydata = async (req, res) => {
   }
 };
 
-const commentdata=async(req,res)=>{
+const displaydata = async (req, res) => {
   const t = await db.sequelize.transaction();
   try {
-    const studentdata = await student.create({firstname:"Shreya",lastname:"Kanaujia",phone:"9900889900"});
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = (page - 1) * limit;
+    const Data = await student.findAll({
+      include: [
+        {
+          model: enrollment,
+          include: [
+            {
+              model: course,
+              include: {
+                model: professor,
+              },
+            },
+          ],
+        },
+      ],
+      offset: offset,
+      limit: limit,
+    });
+    const totalcount = await student.count();
+    res.json({ Data, totalcount });
+    await t.commit();
+  } catch (error) {
+    res.send(error);
+    await t.rollback();
+  }
+};
 
-    const professordata = await professor.create({name:"Snehal",qualification:"M-tech",course_id:1});
+const handleSort = async (req, res) => {
+  try {
+    const { column, sortDirection } = req.query;
+    let { page, limit } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 5;
+    const offset = (page - 1) * limit;
+    const sortingColumn = column === "id" ? "id" : column;
+    const orderOption = column && sortDirection ? [[sortingColumn, sortDirection]] : [];
+    const searchData = await student.findAll({
+      include: [
+        {
+          model: enrollment,
+          include: [
+            {
+              model: course,
+              include: {
+                model: professor,
+              },
+            },
+          ],
+        },
+      ],
+      offset: offset,
+      limit: limit,
+      order: orderOption,
+    });
+    const totalcount = await student.count();
+    res.json({searchData,totalcount});
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-    if(studentdata && studentdata.id)
-    {
-        await comment.create({comment:"good",commentid:studentdata.id,commmentype:"student"});
+const commentdata = async (req, res) => {
+  const t = await db.sequelize.transaction();
+  try {
+    const studentdata = await student.create({
+      firstname: "Shreya",
+      lastname: "Kanaujia",
+      phone: "9900889900",
+    });
+
+    const professordata = await professor.create({
+      name: "Snehal",
+      qualification: "M-tech",
+      course_id: 1,
+    });
+
+    if (studentdata && studentdata.id) {
+      await comment.create({
+        comment: "good",
+        commentid: studentdata.id,
+        commmentype: "student",
+      });
     }
-    if(professordata && professordata.id)
-    {
-        await comment.create({comment:"best",commentid:professordata.id,commmentype:"professor"});
+    if (professordata && professordata.id) {
+      await comment.create({
+        comment: "best",
+        commentid: professordata.id,
+        commmentype: "professor",
+      });
     }
 
     res.send(studentdata);
@@ -118,23 +168,24 @@ const commentdata=async(req,res)=>{
     res.send(error);
     await t.rollback();
   }
-}
+};
 
-const displaycomments=async(req,res)=>{
+const displaycomments = async (req, res) => {
   const t = await db.sequelize.transaction();
   try {
-    const data=await student.findAll({include:{model:comment}})
+    const data = await student.findAll({ include: { model: comment } });
     res.send(data);
     await t.commit();
   } catch (error) {
     res.send(error);
     await t.rollback();
   }
-}
+};
 
 module.exports = {
   displaydata,
   searchdata,
   commentdata,
   displaycomments,
+  handleSort,
 };
--- routes/student.router.js
+++ routes/student.router.js
@@ -1,8 +1,10 @@
 const studentcontroller = require("../controllers/student.controller.js");
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/displaydata', studentcontroller.displaydata);
-router.get('/searchdata/:search', studentcontroller.searchdata);
-router.post('/commentdata', studentcontroller.commentdata);
-router.get('/displaycomments',studentcontroller.displaycomments);
+router.get("/displaydata", studentcontroller.displaydata);
+router.get("/searchdata/:search", studentcontroller.searchdata);
+router.post("/commentdata", studentcontroller.commentdata);
+router.get("/displaycomments", studentcontroller.displaycomments);
+router.get("/handleSort", studentcontroller.handleSort);
+
 module.exports = router;
